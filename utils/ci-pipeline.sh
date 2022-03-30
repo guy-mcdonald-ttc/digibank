9f92ba1d1869c3e43b9bd281e09fb6353b3a2eda
Fabian Streitel <streitel@cqse.eu>
2022-03-30 21:00:42 +0200

    run tests headless and instrument integration tests

--- utils/ci-pipeline.sh
+++ utils/ci-pipeline.sh
@@ -1,4 +1,5 @@
 # build and test app
-./mvnw clean verify
+export MOZ_HEADLESS=1 # run tests headless
+(cd bank; ./mvnw clean verify)
 # upload test coverage to Teamscale
 ./utils/teamscale-upload -s http://localhost:8080 -u build -a anqqeNLeiHgeyQ43DGq1ks9OZZFMYho9 -p digibank -t 'Automated Tests' -f jacoco '**/jacoco.xml'
