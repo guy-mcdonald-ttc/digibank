7e60d26bce8ab7a6f77161cc2d1bb06ed1dc5e6a
Fabian Streitel <streitel@cqse.eu>
2022-03-30 21:27:11 +0200

    upload unit and integration test coverage separately
    and update TS backup

--- utils/ci-pipeline.sh
+++ utils/ci-pipeline.sh
@@ -2,4 +2,5 @@
 export MOZ_HEADLESS=1 # run tests headless
 (cd bank; ./mvnw clean verify)
 # upload test coverage to Teamscale
-./utils/teamscale-upload -s http://localhost:8080 -u build -a anqqeNLeiHgeyQ43DGq1ks9OZZFMYho9 -p digibank -t 'Automated Tests' -f jacoco '**/jacoco.xml'
+./utils/teamscale-upload -s http://localhost:8080 -u build -a anqqeNLeiHgeyQ43DGq1ks9OZZFMYho9 -p digibank -t 'Unit Tests' -f jacoco '**/jacoco/jacoco.xml'
+./utils/teamscale-upload -s http://localhost:8080 -u build -a anqqeNLeiHgeyQ43DGq1ks9OZZFMYho9 -p digibank -t 'Integration Tests' -f jacoco '**/jacoco-it/jacoco.xml'
