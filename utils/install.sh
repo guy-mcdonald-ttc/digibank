414cb91929b5ffa957bf5f2f5defe039d2c39478
Thomas Pettinger <pettinger.thomas@gmail.com>
2022-05-03 14:23:05 +0200

    install.sh: Make all branches available locally

--- utils/install.sh
+++ utils/install.sh
@@ -11,4 +11,14 @@ function setup_manual_test() {
   unzip ./utils/manual_test.zip
 }
 
+function setup_git_repo() {
+  branches="$(git branch --all | grep "remotes/origin/" | grep -v "HEAD" | sed 's/remotes\/origin\///')"
+  for branch in $branches; do
+    echo Creating local branch $branch
+    git switch "$branch" >/dev/null 2>&1
+  done
+  git switch master
+}
+
 setup_manual_test
+setup_git_repo
