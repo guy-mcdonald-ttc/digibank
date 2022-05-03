9f4fd4206c440b607d4b429da473bf2d2bc34f63
Thomas Pettinger <pettinger.thomas@gmail.com>
2022-05-03 15:06:24 +0200

    install.sh: exclude teamscale/teamscale/? folder with .git/info/exclude (to prevent another major rebase)

--- utils/install.sh
+++ utils/install.sh
@@ -12,12 +12,20 @@ function setup_manual_test() {
 }
 
 function setup_git_repo() {
+  # Clone all remote branches so that they are available locally
   branches="$(git branch --all | grep "remotes/origin/" | grep -v "HEAD" | sed 's/remotes\/origin\///')"
   for branch in $branches; do
     echo Creating local branch $branch
     git switch "$branch" >/dev/null 2>&1
   done
   git switch master
+
+  # On Ubuntu this folder is created when Teamscale starts.
+  # We need to exclude it from git for the release and issue test scripts to be able to switch branches
+  # This was the easiest way to do that without another major rebase
+  if [[ ! "$(tail -1 .git/info/exclude)" == "\"teamscale/teamscale/?\"" ]]; then
+    echo "\"teamscale/teamscale/?\"" >>.git/info/exclude
+  fi
 }
 
 setup_manual_test
