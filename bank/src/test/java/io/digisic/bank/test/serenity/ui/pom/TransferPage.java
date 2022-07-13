892ee809f27674831f72a055f44a6674d11f5597
Fabian Streitel <streitel@cqse.eu>
2022-07-13 11:47:21 +0200

    add page object for transfer page

---
+++ bank/src/test/java/io/digisic/bank/test/serenity/ui/pom/TransferPage.java
@@ -0,0 +1,47 @@
+package io.digisic.bank.test.serenity.ui.pom;
+
+import net.thucydides.core.annotations.DefaultUrl;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+@DefaultUrl ("https://localhost:8443/account/xfer-between")
+public class TransferPage {
+	
+	@FindBy(id="fromAccount")
+	private WebElement fromAccount;
+	
+	@FindBy(id="toAccount")
+	private WebElement toAccount;
+	
+	@FindBy(id="amount")
+	private WebElement amountInput;
+
+	@FindBy(id="submit")
+	private WebElement submitButton;
+
+	@FindBy(id="error")
+	private WebElement errorAlert;
+
+	public void selectFromAccount(int index) {
+		new Select(fromAccount).selectByIndex(index);
+	}
+
+	public void selectToAccount(int index) {
+		new Select(toAccount).selectByIndex(index);
+	}
+	
+	public void enterAmount(String amount) {
+		amountInput.sendKeys(amount);
+	}
+	
+	public void clickSubmit() {
+		submitButton.click();
+	}
+
+	public boolean expectedTransferBetweenSameAccountErrorDisplayed() {
+		return errorAlert.getText().equals("Can not trasnsfer from and to the same account.");
+	}
+	
+}
